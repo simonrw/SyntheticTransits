c2033eeaee76f18d88e0ed11f60776cdb0296b8d
Simon Walker <s.r.walker101@googlemail.com>
2011-07-07 11:43:05 +0100

    getting object list

--- Analyse.py
+++ Analyse.py
@@ -7,3 +7,12 @@ import sys
 
 if len(sys.argv) != 4:
     raise RuntimeError("Program usage: %s oldfile newfile objectid" % sys.argv[0])
+
+OldFilename = sys.argv[1]
+NewFilename = sys.argv[2]
+ObjectId = sys.argv[3]
+
+OldFilePointer = pyfits.open(OldFilename)
+NewFilePointer = pyfits.open(NewFilename)
+
+ObjectList = OldFilePointer['CATALOGUE'].data.field("OBJ_ID")
