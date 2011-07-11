50f9f7b2886820dd25e9e36283c313d44bf9e45d
Simon Walker <s.r.walker101@googlemail.com>
2011-07-11 11:50:43 +0100

    added header guard to constants.h

--- include/constants.h
+++ include/constants.h
@@ -1,3 +1,6 @@
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
 const double rJup = 71492E3;
 const double rSun = 6.995E8;
 const double AU = 1.496E11;
@@ -6,3 +9,6 @@ const double secondsInHour = 60. * secondsInMinute;
 const double secondsInDay = 24. * secondsInHour;
 const double radiansInDegree = 2. * 3.14 / 360.;
 const double degreesInRadian = 360. / 2. / 3.14;
+
+
+#endif // CONSTANTS_H_
