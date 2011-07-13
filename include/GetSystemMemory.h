2707a837243cbc6d35cdeae563ebd031b2c8c12d
Simon Walker <s.r.walker101@googlemail.com>
2011-07-13 14:15:04 +0100

    added doxygen docstring for system memory funciton

--- include/GetSystemMemory.h
+++ include/GetSystemMemory.h
@@ -4,6 +4,12 @@
 
 #define GETSYSTEMMEMORY_H
 
+/** Returns the system memory
+ *
+ * Runs platform independant (on apple and linux anyway)
+ * method of getting the system memory (RAM) in bytes
+ *
+ */
 long getTotalSystemMemory();
 
 
