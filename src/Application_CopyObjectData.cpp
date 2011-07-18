c0f131c862cffa6d3a50d102d919469ff59a7c47
Simon Walker <s.r.walker101@googlemail.com>
2011-07-18 15:22:08 +0100

    pasted in code for copying catalogue data

--- src/Application_CopyObjectData.cpp
+++ src/Application_CopyObjectData.cpp
@@ -6,9 +6,63 @@
 using namespace std;
 using namespace CCfits;
 
+typedef map<string, Column*> ColumnList;
+
 /** Copies an object at given index to another location in the file
  *
  * @param LocationIndex Empty lightcurve location in the file */
 void Application::CopyObject(const int LocationIndex)
 {
+    /*  things to retrieve:
+     *      - Catalogue entries
+     *      - Image data in a strip
+     */
+    ExtHDU &CatalogueHDU = mInfile->extension("CATALOGUE");
+    ColumnList Columns = CatalogueHDU.column();
+
+    for (ColumnList::const_iterator i=Columns.begin();
+            i!=Columns.end();
+            ++i)
+    {
+
+        /*  get the data type */
+        string Format = i->second->format();
+
+        if (Format == "1J")
+        {
+            /*  data type is long */
+            vector<long> data;
+            i->second->read(data, mObjectIndex + 1, 1);
+
+        }
+        else if (Format == "1I")
+        {
+            /*  data type is int */
+            vector<int> data;
+            i->second->read(data, mObjectIndex + 1, 1);
+        }
+        else if (Format == "1E")
+        {
+            /*  data type is float */
+            vector<float> data;
+            i->second->read(data, mObjectIndex + 1, 1);
+        }
+        else if (Format == "26A")
+        {
+            /*  data type is string */
+            vector<string> data;
+            i->second->read(data, mObjectIndex + 1, 1);
+        }
+        else if (Format == "1D")
+        {
+            /*  data type is double */
+            vector<double> data;
+            i->second->read(data, mObjectIndex + 1, 1);
+        }
+        else
+        {
+            cerr << "Unknown data type: " << Format << endl;
+        }
+    }
+
 }
