4c43696ef22a5f2774ee926150aec6b234ac9777
Simon Walker <s.r.walker101@googlemail.com>
2011-07-18 17:37:10 +0100

    one update function calling the other

--- src/Application_UpdateFile.cpp
+++ src/Application_UpdateFile.cpp
@@ -3,27 +3,8 @@
 using namespace std;
 using namespace CCfits;
 
-/** Update the open file with the new Lightcurve object
- *
- * First the flux must be obtained from the lightcurve object, 
- * and then written in place of the old data for the selected 
- * object
- *
- */
-void Application::UpdateFile(const Lightcurve &lc)
-{
-    ExtHDU &fluxHDU = mInfile->extension("FLUX");
-    const long nFrames = fluxHDU.axis(0);
-    
-    /*  have to create a valarray for writing */
-    valarray<double> writeArray(nFrames);
-    for (int i=0; i<nFrames; ++i) writeArray[i] = lc.flux[i];
-    
-    long firstElement = mObjectIndex * nFrames + 1;
-    fluxHDU.write(firstElement, nFrames, writeArray);
 
-    
-}
+
 
 /** Update the open file at a particular location */
 void Application::UpdateFile(const Lightcurve &lc, const int TargetIndex)
@@ -40,5 +21,18 @@ void Application::UpdateFile(const Lightcurve &lc, const int TargetIndex)
     long firstElement = TargetIndex * nFrames + 1;
     fluxHDU.write(firstElement, nFrames, writeArray);
 
+    
+}
+
+/** Update the open file with the new Lightcurve object
+ *
+ * First the flux must be obtained from the lightcurve object, 
+ * and then written in place of the old data for the selected 
+ * object
+ *
+ */
+void Application::UpdateFile(const Lightcurve &lc)
+{
+    UpdateFile(lc, mObjectIndex);
     
 }
