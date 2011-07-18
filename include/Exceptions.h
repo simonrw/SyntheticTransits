51320f70beb37b53e6cfdf44978490a1306ad469
Simon Walker <s.r.walker101@googlemail.com>
2011-07-18 13:57:25 +0100

    added cfitsio exception

--- include/Exceptions.h
+++ include/Exceptions.h
@@ -24,6 +24,32 @@ struct BaseException : public std::exception
     const char *what() const throw() { return str.c_str(); }
 };
 
+/** cfitsio specific exception
+ *
+ * Takes a cfitsio status integer and the FitsioException::what() function 
+ * returns the cfitiso error message
+ *
+ * @param[in] status cfitsio status number
+ */
+class FitsioException : public BaseException
+{
+    public:
+        FitsioException(const int status) : BaseException(""), status(status) {}
+        virtual ~FitsioException() throw() {}
+        const char *what() const throw()
+        {
+            fits_get_errstatus(status, (char*)errmsg);
+            return errmsg;
+        }
+
+        int GetStatus() const { return this->status; }
+
+
+    private:
+        int status;
+        char errmsg[FLEN_STATUS];
+};
+
 
 /** Thrown if the selected object is not in the file */
 struct ObjectNotFound : public BaseException
