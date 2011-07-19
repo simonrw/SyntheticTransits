f1fc09c9a59e4835bdd301e3449de8f0592cb5ee
Simon Walker <s.r.walker101@googlemail.com>
2011-07-19 11:44:39 +0100

    added guts of function

--- src/ValidXML.cpp
+++ src/ValidXML.cpp
@@ -5,4 +5,17 @@ using namespace std;
 
 bool ValidXML(const string &filename)
 {
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file(filename.c_str());
+
+    if (result)
+    {
+        /*  file is valid */
+        return true;
+    }
+    else
+    {
+        /*  file is not valid */
+        return false;
+    }
 }
