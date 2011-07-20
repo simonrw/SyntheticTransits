93771fcb2dcb0ce09abfcc16c007d242b1229bf7
Simon Walker <s.r.walker101@googlemail.com>
2011-07-20 10:42:12 +0100

    added if clause to documentation updater

--- UpdateDocumentation.sh
+++ UpdateDocumentation.sh
@@ -4,9 +4,14 @@
 # MUST BE CALLED FROM THE ROOT DIRECTORY
 #
 
-doxygen
-git co gh-pages
-cp -r docs/html/* .
-git commit -am 'updated documentation'
-git push origin gh-pages
-git co master
+if [ -f "Doxyfile" ]; then
+    doxygen
+    git co gh-pages
+    cp -r docs/html/* .
+    git commit -am 'updated documentation'
+    git push origin gh-pages
+    git co master
+else
+    echo >&2 "Error cannot find Doxyfile in current directory. Exiting"
+    exit 1
+fi
