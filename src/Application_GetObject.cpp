dcd672aa4b7b6b93ab0a098d8f128b25e4ac2ae9
Simon Walker <s.r.walker101@googlemail.com>
2011-08-22 14:50:52 +0100

    setting the object id

--- src/Application_GetObject.cpp
+++ src/Application_GetObject.cpp
@@ -1,6 +1,9 @@
 #include "Application.h"
+#include <sstream>
+#include <CCfits/CCfits>
 
 using namespace std;
+
 
 Lightcurve Application::getObject()
 {
@@ -16,6 +19,25 @@ Lightcurve Application::getObject()
         returnval.jd[i] = hjd[i];
         returnval.fluxerr[i] = fluxerr[i];
     }
+
+    /* Get the object id */
+    CCfits::Column &ObjIDCol = mInfile->extension("CATALOGUE").column("OBJ_ID");
+    int ColumnType = ObjIDCol.type();
+    if (ColumnType == CCfits::Tlong)
+    {
+        vector<long> Names;
+        ObjIDCol.read(Names, mObjectIndex+1, mObjectIndex+1);
+        stringstream ss;
+        ss << Names[0];
+        returnval.obj_id = ss.str();
+    }
+    else if (ColumnType == CCfits::Tstring)
+    {
+        vector<string> Names;
+        ObjIDCol.read(Names, mObjectIndex+1, mObjectIndex+1);
+        returnval.obj_id = Names[0];
+    }
+
     
     return returnval;
 
