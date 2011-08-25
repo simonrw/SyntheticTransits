5d1d01752642d28d6831a106ae54efb003bd0cdf
Simon Walker <s.r.walker101@googlemail.com>
2011-08-25 14:52:13 +0100

    changed default output file name
    now synthout.fits

--- src/Application_Go.cpp
+++ src/Application_Go.cpp
@@ -107,7 +107,7 @@ int Application::go(int argc, char *argv[])
     TCLAP::CmdLine cmd("Synthetic lightcurves", ' ', "1.0");
     TCLAP::ValueArg<float> memlimit_arg("M", "memorylimit", "Fraction of system memory to use", false, 0.1, "0-1", cmd);
     TCLAP::SwitchArg wasptreatment_arg("w", "wasp", "Do not treat as WASP object", cmd, true);
-    TCLAP::ValueArg<string> output_arg("o", "output", "Optional output file", false, "output.fits", "Fits filename", cmd);
+    TCLAP::ValueArg<string> output_arg("o", "output", "Optional output file", false, "synthout.fits", "Fits filename", cmd);
     //    TCLAP::ValueArg<string> objectid_arg("O", "object", "Object to alter", true, "", "Object identifier", cmd);
     TCLAP::ValueArg<string> subModel_arg("s", "submodel", "Model to subtract", true, "", "Model xml file", cmd);
     TCLAP::ValueArg<string> addModelFilename_arg("a", "addmodels", "List of model files", true, "", "List of files", cmd);
